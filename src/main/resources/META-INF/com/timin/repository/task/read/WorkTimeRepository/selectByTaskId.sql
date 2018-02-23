913d3accce673e2450503c92e47311b5eee513a1
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2018-02-24 00:55:01 +0900

    修正

--- src/main/resources/META-INF/com/timin/repository/task/read/WorkTimeRepository/selectByTaskId.sql
+++ src/main/resources/META-INF/com/timin/repository/task/read/WorkTimeRepository/selectByTaskId.sql
@@ -2,4 +2,7 @@ SELECT *
 FROM work_time
 WHERE
     task_id = /*taskId */1
-    AND `out` >= /*now*/'2017-01-01 00:00:00';
+      AND data_in <= /* now */'2017-01-01 00:00:00'
+      AND data_out >= /* now */'2017-01-01 00:00:00'
+      AND data_thru >= /* now */'2017-01-01 00:00:00'
+      AND data_from <= /* now */'2017-01-01 00:00:00'
